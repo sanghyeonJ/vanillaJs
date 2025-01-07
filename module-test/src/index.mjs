e48cbd962e96f1109cef5da47a6e8e93b8996be3
sanghyeonJ <jsh080000@gmail.com>
2025-01-07 20:04:46 +0900

    250107 스카

---
+++ module-test/src/index.mjs
@@ -0,0 +1,5 @@
+import {num, add, User} from './test.mjs';
+
+console.log(num);
+console.log(add(10, 10));
+console.log(new User('홍길동'));
---
+++ module-test/src/test.mjs
@@ -0,0 +1,9 @@
+export const num = 35;
+
+export function add(num1, num2){
+    return num1 + num2;
+}
+
+export function User(name){
+    this.name = name;
+}
